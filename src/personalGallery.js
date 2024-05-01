b801ae4d7fae4de2adf9b7f5c8ede3a0a1dc78d2
Andrew Oh <aaoh@usc.edu>
2024-04-30 22:11:59 -0700

    Added view postcard functionality so that card flips when clicked. Don't know if proper message will display until other functions are implemented.

--- src/personalGallery.js
+++ src/personalGallery.js
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import './personalGallery.css';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+import { useRef } from 'react';
 
 const PersonalGallery = () =>{
 
     const[gallery, setPersonalGallery] = useState([]);
+    const [isFlipped, setIsFlipped] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
 
     useEffect(() =>{
         const userProfile = JSON.parse(localStorage.getItem('userProfile'));
@@ -29,19 +32,37 @@ const PersonalGallery = () =>{
             // Handle click event
             // setSelectedImageIndex(index);
 
+            const newGallery = gallery.map((item, i) => {
+                if (i === index) {
+                    return { ...item, clicked: !item.clicked };
+                } else {
+                    return { ...item, clicked: false };
+                }
+            });
+            setSelectedImage(newGallery[index].clicked ? index : null);
+
             const galleryElements = document.querySelectorAll('.gallery-container');
             galleryElements.forEach(element => {
                 element.style.display = 'none';
             });
 
             const viewElements = document.querySelectorAll('.viewPostcard-container');
-            viewElements.forEach(element => {
+            viewElements.forEach(viewElement => {
+                const galleryElement = viewElement.querySelector('.viewGallery');
+                viewElement.style.display = 'block'; // Hide the viewPostcard-container
+            });
+
+
+            const backButton = document.querySelectorAll('.go-back');
+            backButton.forEach(element => {
                 element.style.display = 'block';
             });
         };
+
+        const prevCount = useRef(1);
 
         const handleGoBack = () => {
-            // setSelectedImageIndex(null);
+            setSelectedImage(null);
     
             const galleryElements = document.querySelectorAll('.gallery-container');
             galleryElements.forEach(element => {
@@ -49,10 +70,53 @@ const PersonalGallery = () =>{
             });
     
             const viewElements = document.querySelectorAll('.viewPostcard-container');
-            viewElements.forEach(element => {
+            viewElements.forEach(viewElement => {
+                const galleryElement = viewElement.querySelector('.viewGallery');
+                // galleryElement.classList.remove('flip'); // Remove the 'flip' class from the gallery element
+                viewElement.style.display = 'none'; // Hide the viewPostcard-container
+            });
+
+            const backButton = document.querySelectorAll('.go-back');
+            backButton.forEach(element => {
                 element.style.display = 'none';
             });
         };
+
+        // const prevCount = useRef(1);
+
+        const handleImageClick = () => {
+            setIsFlipped(!isFlipped);
+            prevCount.current += 1;
+            console.log("Count: " + prevCount.current);
+            
+            const viewPostcardContainer = document.querySelector('.viewGallery');
+            viewPostcardContainer.classList.toggle('flip'); // Toggle the 'flip' class
+            
+            const frontCard = document.querySelectorAll('.front');
+            const backCard = document.querySelectorAll('.back');
+            
+            if (prevCount.current % 2 === 0) {
+                frontCard.forEach(element => {
+                    element.style.display = 'none';
+                });
+
+                backCard.forEach(element => {
+                    element.style.display = 'flex';
+                    console.log("back is shown");
+                });
+            } else {
+                frontCard.forEach(element => {
+                    element.style.display = 'flex';
+                });
+
+                backCard.forEach(element => {
+                    element.style.display = 'none';
+                    console.log("back is hidden");
+                });
+            }
+        };
+
+        
     
         const baseUrl = "https://file.postexchange.icytools.cn/img/";
 
@@ -73,18 +137,39 @@ const PersonalGallery = () =>{
                 </div>
             </div>
             {/* <div className="viewPostcard-container" style={{ display: selectedImageIndex !== null ? 'block' : 'none' }}> */}
-            <div className="viewPostcard-container" style={({display: 'none'})}>
+            <div className="viewPostcard-container" style={{ display: 'none' }}>
+                <div className="viewGallery" onClick={handleImageClick}>
+                    {/* userReceived = {gallery[selectedImage].userIDReceived}
+                            userSent = {gallery[selectedImage].userIDSent}
+                            dateSent = {gallery[selectedImage].timeSent}
+                            postcardPicture = {gallery[selectedImage].postcardImage}
+                            postcardMsg = {gallery[selectedImage].postcardMessage} */}
+                    <div className="front">
+                        <img src={baseUrl + `J49rFQpLHw.jpeg`} alt="Front of postcard" />
+                    </div>
+                    <div className="back" style={{display:'none'}} >
+                        <div className="backInfo">
+                            <p><b>Date Sent: </b><span style={{ textDecoration: 'underline' }}>{dateSent}</span></p>
+                            <p><b>To: </b><span style={{ textDecoration: 'underline' }}>{userReceived}</span></p>
+                            <p><b>From: </b><span style={{ textDecoration: 'underline' }}>{userSent}</span></p>
+                            
 
-                
-                <img src={baseUrl + `J49rFQpLHw.jpeg`}></img>
-                <button className="go-back" onClick={handleGoBack}>Go Back</button>
-                {/* {selectedImageIndex !== null && (
-                    <>
-                        {<img src={baseUrl + `J49rFQpLHw.jpeg`} alt={gallery[selectedImageIndex].img} />}
-                        <button onClick={handleGoBack}>Go Back</button>
-                    </>
-                )} */}
+                        </div>
+                        <div className="verticalLine"></div>
+                        <div className="backMessage">
+                            <p><b>Postcard Message</b></p>
+                            <p style={{ textDecoration: 'underline' }}>{postcardMsg}</p>
+                        </div>
+                    </div>
+                    <div className="revealPostcardMsg">
+                        <p><b>Click to Reveal Postcard Content!</b></p>
+                    </div>
+                </div>
             </div>
+            <button className="go-back" style={{display:'none'}} onClick={handleGoBack}>Go Back</button>
+            
+            
+
         </div>
         )
 }
