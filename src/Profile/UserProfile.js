3ee3faf3ec4f141d8ec568800dd79403679ba67b
ajia117 <ajia.117@gmail.com>
2024-05-03 23:07:12 -0700

    revise login/userprofile to include credentials when logging in

--- src/Profile/UserProfile.js
+++ src/Profile/UserProfile.js
@@ -12,9 +12,32 @@ const UserProfile = ({loginToken, setLoginToken, setLoginState}) => {
     e.preventDefault();
     setLoginToken(null);
     setLoginState(false);
+    fetch('https://postexchange.icytools.cn/doLogout', {
+      method: 'GET',
+      credentials: 'include',
+    });
     localStorage.setItem('userProfile', null);
     sessionStorage.setItem('userProfile', null);
     navigate('/');
+  };
+
+  const randUser = async e => {
+    try {
+      const response = await fetch(`https://postexchange.icytools.cn/getRandUser`, {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        throw new Error('Login error');
+      }
+
+      const logResp = await response.json();
+      console.log('Success:', logResp);
+    } catch (error) {
+
+    }
   };
 
   return (
@@ -22,6 +45,9 @@ const UserProfile = ({loginToken, setLoginToken, setLoginState}) => {
       <div>User Profile Page (in separate folder)</div>
       <Button variant="primary" type="submit" onClick={handleSumbit}>
         Logout
+      </Button>
+      <Button type="submit" onClick={randUser}>
+        Random User
       </Button>
     </Container>
   );
