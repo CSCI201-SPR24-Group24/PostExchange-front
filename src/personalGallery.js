6b4e945b8b3c208f1bb37f0b4925d2b7165e8384
Andrew Oh <aaoh@usc.edu>
2024-05-06 20:19:50 -0700

    removed messages from personal gallery

--- src/personalGallery.js
+++ src/personalGallery.js
@@ -1,54 +1,3 @@
-// import { useEffect } from "react";
-// import './personalGallery.css';
-// import { useState } from "react";
-
-
-// const PersonalGallery = () =>{
-
-//     const[gallery, setPersonalGallery] = useState([]);
-
-//     useEffect(() =>{
-//         const userProfile = JSON.parse(localStorage.getItem('userProfile'));
-
-//         if(userProfile){
-//             fetch("https://postexchange.icytools.cn/getPersonalGallery",
-//         {credentials: 'include'}
-//     )
-//             .then(response => response.json())
-//             .then(data => {
-//                 setPersonalGallery(data.data);
-//                 console.log(data);
-//             })
-//         }
-
-
-//         },[]);
-
-//         const baseUrl = "https://file.postexchange.icytools.cn/img/";
-
-//         return(
-//            <div className="home-container">
-//           <div className="gallery-container">
-//             <h2>Your Gallery</h2>
-//                 <div className="gallery">
-//                     {gallery.map((item, index) => (
-//                         <div key={index} className="gallery-item">
-//                             <img src={baseUrl + `J49rFQpLHw.jpeg`} alt={item.img} />
-//                             <div className="number">
-//                                 <h3></h3>
-//                                 </div> {/* Separate div for numbering */}
-//                             </div>
-//                     ))}
-//                 </div>
-    
-    
-//           </div>
-//         </div>
-//         )
-// }
-
-// export default PersonalGallery;
-
 import { useEffect } from "react";
 import './personalGallery.css';
 import { useState } from "react";
@@ -68,12 +17,6 @@ const PersonalGallery = () =>{
     const navigate = useNavigate();
     const prevCount = useRef(1);
 
-    // var userReceived;
-    // var userSent;
-    // var dateSent;
-    // var postcardPicture;
-    // var postcardMsg;
-
     useEffect(() => {
         const userProfile = JSON.parse(localStorage.getItem('userProfile'));
 
@@ -89,8 +32,6 @@ const PersonalGallery = () =>{
     
 
     const handleClick = (index, item) => {
-        // Handle click event
-        // setSelectedImageIndex(index);
 
         const newGallery = gallery.map((item, i) => {
             if (i === index) {
@@ -99,7 +40,6 @@ const PersonalGallery = () =>{
                 return { ...item, clicked: false };
             }
         });
-        // setSelectedImage(newGallery[index].clicked ? index : null);
         setSelectedImage(item);
 
         const galleryElements = document.querySelectorAll('.gallery-container');
@@ -133,7 +73,6 @@ const PersonalGallery = () =>{
         const viewElements = document.querySelectorAll('.viewPostcard-container');
         viewElements.forEach(viewElement => {
             const galleryElement = viewElement.querySelector('.viewGallery');
-            // galleryElement.classList.remove('flip'); // Remove the 'flip' class from the gallery element
             viewElement.style.display = 'none'; // Hide the viewPostcard-container
         });
 
@@ -143,7 +82,6 @@ const PersonalGallery = () =>{
         });
     };
 
-    // const prevCount = useRef(1);
 
     const handleImageClick = (image) => {
         setIsFlipped(!isFlipped);
@@ -194,7 +132,6 @@ const PersonalGallery = () =>{
             <div className="gallery">
                 {gallery.map((item, index) => (
                     <div key={index} className="gallery-item" onClick={() => handleClick(index, item)}> {/* Attach onClick event handler */}
-                    {/* <div key={index} className="gallery-item"> */}
                         <img src={baseUrl + `J49rFQpLHw.jpeg`} alt={item.img} />
                         <div className="number">
                             <h3></h3>
@@ -203,9 +140,7 @@ const PersonalGallery = () =>{
                 ))}
             </div>
         </div>
-        {/* <div className="viewPostcard-container" style={{ display: selectedImageIndex !== null ? 'block' : 'none' }}> */}
         <div className="viewPostcard-container" style={{ display: 'none' }}>
-            {/* <div className="viewGallery" onClick={handleImageClick(gallery[selectedImage])}> */}
             <div className="viewGallery" onClick={() => handleImageClick(selectedImage)}>
                 <div className="front">
                     <img src={baseUrl + `J49rFQpLHw.jpeg`} alt="Front of postcard" />
@@ -213,13 +148,11 @@ const PersonalGallery = () =>{
                 <div className="back" style={{display:'none'}} >
                     <div className="backInfo">
                     <p><b>Date Sent: </b><span style={{ textDecoration: 'underline' }}>{dateSent}</span></p>
-                    <p><b>To: </b><span style={{ textDecoration: 'underline' }}>{userReceived}</span></p>
-                    <p><b>From: </b><span style={{ textDecoration: 'underline' }}>{userSent}</span></p>
                     </div>
                     <div className="verticalLine"></div>
                     <div className="backMessage">
-                        <p><b>Postcard Message</b></p>
-                        <p style={{ textDecoration: 'underline' }}>{postcardMsg}</p>
+                    <p><b>To: </b><span style={{ textDecoration: 'underline' }}>{userReceived}</span></p>
+                    <p><b>From: </b><span style={{ textDecoration: 'underline' }}>{userSent}</span></p>
                     </div>
                 </div>
                 <div className="revealPostcardMsg">
